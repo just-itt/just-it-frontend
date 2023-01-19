16dc3165193ad37f838826447fd0b0ee40ed87b8
choisuhyeok1255 <csh300029@gmail.com>
2023-01-19 18:30:19 +0900

    prettierrc 설정

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80, //코드 한줄 최대치
+  semi: true, //코드 마지막에 세미콜론
+  singleQuote: false, //문자열을 작은따옴표로 작성할것인지(false = 큰 따옴표)
+  trailingComma: "all", //객체나 배열 등에 맨 마지막에도 콤마
+  tabWidth: 2, //들여쓰기 2칸(스페이스 2칸)
+  bracketSpacing: true, //객체 리터럴에서 괄호에 공백 삽입 여부
+  endOfLine: "auto",
+  useTabs: false, //탭 대신 스페이스
+  arrowParens: "avoid", // 화살표 함수에서 매개변수를 하나만 받을때 괄호 생략
+};
