cc3776aab9294b20219e4718dfd8f362918b38e8
choisuhyeok1255 <csh300029@gmail.com>
2023-01-25 23:34:30 +0900

    Main page 생성

---
+++ pages/main/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Pheed } from "@components/index";
+import * as S from "./main.styled";
+
+const index = () => {
+  return (
+    <S.Main>
+      <Pheed />
+      <Pheed />
+      <Pheed />
+      <Pheed />
+      <Pheed />
+      <Pheed />
+    </S.Main>
+  );
+};
+
+export default index;
---
+++ pages/main/main.styled.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Main = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  row-gap: 40px;
+  max-width: 1128px;
+  margin: 0 auto;
+`;
