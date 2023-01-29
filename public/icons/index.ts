0360a55e64ccc823e447c264ab55b403df4b4659
choisuhyeok1255 <csh300029@gmail.com>
2023-01-29 15:36:14 +0900

    arrow, clip, save, search icon 추가

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,2 +1,6 @@
 export { default as HeartIcon } from "./heart.svg";
 export { default as TalkIcon } from "./talk.svg";
+export { default as ArrowIcon } from "./arrow.svg";
+export { default as SearchIcon } from "./search.svg";
+export { default as ClipIcon } from "./clip.svg";
+export { default as SaveIcon } from "./save.svg";
