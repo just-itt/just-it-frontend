6253708a3555da2df10f8051d4beb44ebe203fd3
choisuhyeok1255 <csh300029@gmail.com>
2023-02-03 00:14:48 +0900

    theme breakPoint 추가

---
+++ styles/breakPoint.ts
@@ -0,0 +1,7 @@
+export const breakPoint = {
+  minMobile: "360px",
+  maxMobile: "727px",
+  minTablet: "727.1px",
+  maxTablet: "1279.9px",
+  minDesktop: "1280px",
+};
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,9 +1,11 @@
 import { color } from "./color";
 import { font } from "./font";
 import { zIndex } from "./zIndex";
+import { breakPoint } from "./breakPoint";
 
 export const theme = {
   color,
   font,
   zIndex,
+  breakPoint,
 };
