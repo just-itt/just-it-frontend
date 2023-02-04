9f584755d24cacf9ef87ea36442075d0dd2cb54b
choisuhyeok1255 <csh300029@gmail.com>
2023-02-04 16:54:44 +0900

    로그인 페이지 스크린 제작

---
+++ pages/login/Login.styled.ts
@@ -0,0 +1,135 @@
+import Link from "next/link";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const Layout = styled.section`
+  ${({ theme }) => css`
+    ${theme.flexMixin.flex_column_justifyC};
+    width: 390px;
+    height: 100vh;
+    margin: 0 auto;
+    padding: 0 20px;
+  `}
+`;
+
+export const LogoWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.flexMixin.flex_justifyC};
+    margin-bottom: 52px;
+  `}
+`;
+
+export const InputWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.flexMixin.flex_column};
+    row-gap: 12px;
+    margin-bottom: 40px;
+  `}
+`;
+
+export const FlexWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.flexMixin.flex_justifySb_alignC};
+    margin-bottom: 20px;
+  `}
+`;
+
+export const CheckboxWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.flexMixin.flex_alignC};
+  `}
+`;
+
+export const KeepLogin = styled.label`
+  ${({ theme }) => css`
+    ${theme.font.regular_14};
+    margin-left: 8px;
+    color: ${theme.color.grey_600};
+  `}
+`;
+
+export const ResetPassword = styled(Link)`
+  ${({ theme }) => css`
+    ${theme.font.regular_14};
+    color: ${theme.color.grey_600};
+    text-decoration: underline;
+  `}
+`;
+
+export const LoginBtn = styled.button`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 52px;
+    margin-bottom: 40px;
+    border-radius: 5px;
+    color: ${theme.color.white};
+    background-color: ${theme.color.blue_200};
+  `}
+`;
+
+export const Or = styled.span`
+  ${({ theme }) => css`
+    ${theme.font.regular_14};
+    ${theme.flexMixin.flex_justifyC_alignC};
+    height: 20px;
+    margin-bottom: 40px;
+    color: ${theme.color.grey_700};
+
+    ::before {
+      content: "";
+      display: block;
+      width: 150px;
+      height: 1px;
+      margin-right: 12px;
+      border-top: 1px solid ${theme.color.grey_300};
+    }
+
+    ::after {
+      content: "";
+      display: block;
+      width: 150px;
+      height: 1px;
+      margin-left: 12px;
+      border-top: 1px solid ${theme.color.grey_300};
+    }
+  `}
+`;
+
+export const SocialLoginBtnWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.flexMixin.flex_justifySb};
+    margin-bottom: 40px;
+    padding: 0 13px;
+  `}
+`;
+
+export const SocialLoginBtn = styled.button`
+  ${({ theme }) => css`
+    ${theme.flexMixin.flex_column_alignC};
+    row-gap: 8px;
+    color: ${theme.color.grey_500};
+  `}
+`;
+
+export const SignUpWrapper = styled.button`
+  ${({ theme }) => css`
+    ${theme.flexMixin.flex};
+    column-gap: 8px;
+    margin: 0 auto;
+  `}
+`;
+
+export const SignUpQuestion = styled.span`
+  ${({ theme }) => css`
+    ${theme.font.regular_14};
+    color: ${theme.color.grey_600};
+  `}
+`;
+
+export const SignUp = styled(Link)`
+  ${({ theme }) => css`
+    ${theme.font.regular_14};
+    color: ${theme.color.grey_600};
+    text-decoration: underline;
+  `}
+`;
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,58 @@
 import React from "react";
 
+import { AuthInput, CheckBox } from "@components/index";
+import {
+  AppleLoginIcon,
+  GoogleLoginIcon,
+  KakaoLoginIcon,
+  LogoIcon,
+  NaverLoginIcon,
+} from "@icons/index";
+import * as S from "./Login.styled";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <S.Layout>
+      <S.LogoWrapper>
+        <LogoIcon />
+      </S.LogoWrapper>
+      <S.InputWrapper>
+        <AuthInput placeholder="이메일" />
+        <AuthInput placeholder="비밀번호" />
+      </S.InputWrapper>
+      <S.FlexWrapper>
+        <S.CheckboxWrapper>
+          <CheckBox />
+          <S.KeepLogin>로그인 상태 유지</S.KeepLogin>
+        </S.CheckboxWrapper>
+        <S.ResetPassword href="">비밀번호 찾기</S.ResetPassword>
+      </S.FlexWrapper>
+      <S.LoginBtn>로그인</S.LoginBtn>
+      <S.Or>또는</S.Or>
+      <S.SocialLoginBtnWrapper>
+        <S.SocialLoginBtn type="button">
+          <GoogleLoginIcon />
+          구글
+        </S.SocialLoginBtn>
+        <S.SocialLoginBtn type="button">
+          <KakaoLoginIcon />
+          카카오
+        </S.SocialLoginBtn>
+        <S.SocialLoginBtn type="button">
+          <NaverLoginIcon />
+          네이버
+        </S.SocialLoginBtn>
+        <S.SocialLoginBtn type="button">
+          <AppleLoginIcon />
+          애플
+        </S.SocialLoginBtn>
+      </S.SocialLoginBtnWrapper>
+      <S.SignUpWrapper>
+        <S.SignUpQuestion>아직 회원이 아니신가요?</S.SignUpQuestion>
+        <S.SignUp href="">회원가입</S.SignUp>
+      </S.SignUpWrapper>
+    </S.Layout>
+  );
 };
 
 export default Login;
