7ee68173eb1a512c3a5c0019bdbf6eed87327555
choisuhyeok1255 <csh300029@gmail.com>
2023-03-11 01:22:26 +0900

    AuthInput 컴포넌트 제작

--- components/domain/login/authInput/AuthInput.component.tsx
+++ components/domain/login/authInput/AuthInput.component.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { HiddenPasswordIcon, ShowPasswordIcon } from "@icons/index";
 import * as S from "./AuthInput.styled";
 
 interface AuthInputProps {
+  autoComplete: "off" | "new-password";
+  type?: string;
   placeholder: string;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const AuthInput = ({ placeholder, handleChange }: AuthInputProps) => {
-  return <S.Input placeholder={placeholder} onChange={handleChange} />;
+const AuthInput = ({
+  autoComplete,
+  type,
+  placeholder,
+  handleChange,
+}: AuthInputProps) => {
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  const handleClickPasswordIcon = () => {
+    setIsShowPassword(!isShowPassword);
+  };
+
+  return (
+    <S.InputWrapper>
+      <S.Label>이메일</S.Label>
+      <S.Input
+        autoComplete={autoComplete}
+        type={isShowPassword ? "text" : type}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+      {type === "password" && (
+        <S.PasswordBtn type="button" onClick={handleClickPasswordIcon}>
+          {isShowPassword ? <ShowPasswordIcon /> : <HiddenPasswordIcon />}
+        </S.PasswordBtn>
+      )}
+    </S.InputWrapper>
+  );
 };
 
 export default AuthInput;
--- components/domain/login/authInput/AuthInput.styled.ts
+++ components/domain/login/authInput/AuthInput.styled.ts
@@ -1,13 +1,30 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
+export const Label = styled.label`
+  position: absolute;
+  top: 8px;
+  left: 12px;
+`;
+
+export const InputWrapper = styled.div`
+  position: relative;
+`;
+
 export const Input = styled.input`
   ${({ theme }) => css`
     ${theme.font.medium_15};
     width: 100%;
-    height: 52px;
+    height: 48px;
     border: 1px solid ${theme.color.grey_300};
     border-radius: 5px;
-    padding: 14px 12px;
+    padding: 30px 12px 12px 12px;
   `}
+`;
+
+export const PasswordBtn = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 12px;
+  transform: translateY(-50%);
 `;
