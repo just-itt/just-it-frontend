0f5d2b7bda4051b5f7fd28c483a10f4115154c10
choisuhyeok1255 <csh300029@gmail.com>
2023-03-25 20:13:21 +0900

    color, font 추가 및 수정

--- styles/font.ts
+++ styles/font.ts
@@ -1,21 +1,57 @@
 import { css } from "@emotion/react";
 
 export const font = {
-  regular_12: css`
-    font-size: 1.2rem;
-    font-weight: 400;
+  bold_20: css`
+    font-size: 2rem;
+    font-weight: 700;
     font-family: Pretendard;
-    line-height: 1.333;
+    line-height: 1.6;
   `,
-  regular_14: css`
-    font-size: 1.4rem;
-    font-weight: 400;
+  bold_18: css`
+    font-size: 1.8rem;
+    font-weight: 700;
     font-family: Pretendard;
-    line-height: 1.428;
+    line-height: 1.6;
   `,
-  regular_15: css`
+  bold_16: css`
+    font-size: 1.6rem;
+    font-weight: 700;
+    font-family: Pretendard;
+    line-height: 1.6;
+  `,
+  bold_15: css`
     font-size: 1.5rem;
-    font-weight: 400;
+    font-weight: 700;
+    font-family: Pretendard;
+    line-height: 1.6;
+  `,
+  semiBold_18: css`
+    font-size: 1.8rem;
+    font-weight: 600;
+    font-family: Pretendard;
+    line-height: 1.5;
+  `,
+  semiBold_16: css`
+    font-size: 1.6rem;
+    font-weight: 600;
+    font-family: Pretendard;
+    line-height: 1.5;
+  `,
+  semiBold_15: css`
+    font-size: 1.5rem;
+    font-weight: 600;
+    font-family: Pretendard;
+    line-height: 1.5;
+  `,
+  semiBold_14: css`
+    font-size: 1.4rem;
+    font-weight: 600;
+    font-family: Pretendard;
+    line-height: 1.5;
+  `,
+  semiBold_13: css`
+    font-size: 1.3rem;
+    font-weight: 600;
     font-family: Pretendard;
     line-height: 1.5;
   `,
@@ -25,22 +61,34 @@ export const font = {
     font-family: Pretendard;
     line-height: 1.6;
   `,
-  medium_18: css`
-    font-size: 1.8rem;
-    font-weight: 600;
+  medium_14: css`
+    font-size: 1.4rem;
+    font-weight: 500;
     font-family: Pretendard;
-    line-height: 1.75;
+    line-height: 1.6;
   `,
-  semiBold_16: css`
-    font-size: 1.6rem;
-    font-weight: 600;
+  medium_13: css`
+    font-size: 1.3rem;
+    font-weight: 500;
+    font-family: Pretendard;
+    line-height: 1.6;
+  `,
+  regular_15: css`
+    font-size: 1.5rem;
+    font-weight: 400;
     font-family: Pretendard;
     line-height: 1.5;
   `,
-  bold_20: css`
-    font-size: 2rem;
-    font-weight: 700;
+  regular_14: css`
+    font-size: 1.4rem;
+    font-weight: 400;
     font-family: Pretendard;
-    line-height: 1.6;
+    line-height: 1.428;
+  `,
+  regular_12: css`
+    font-size: 1.2rem;
+    font-weight: 400;
+    font-family: Pretendard;
+    line-height: 1.333;
   `,
 };
