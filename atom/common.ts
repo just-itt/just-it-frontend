ba8eb928a13c3e2fc69b53031a3ab429cf78c3f8
choisuhyeok1255 <csh300029@gmail.com>
2023-03-25 20:13:04 +0900

    common atom 추가

--- atom/common.ts
+++ atom/common.ts
@@ -1,6 +1,16 @@
 import { atom } from "recoil";
 
-export const isLoginState = atom({
+export const userAtom = atom({
   key: "userState",
+  default: { user: "" },
+});
+
+export const navAtom = atom({
+  key: "navState",
+  default: false,
+});
+
+export const pheedDetailAtom = atom({
+  key: "pheedDetailState",
   default: false,
 });
