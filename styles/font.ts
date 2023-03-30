d0744742a42e30e46c4009478bb91f0c252bd31c
choisuhyeok1255 <csh300029@gmail.com>
2023-03-31 00:10:16 +0900

    font lineheight 설정

--- styles/font.ts
+++ styles/font.ts
@@ -11,49 +11,49 @@ export const font = {
     font-size: 1.8rem;
     font-weight: 700;
     font-family: Pretendard;
-    line-height: 1.6;
+    line-height: 1.5555;
   `,
   bold_16: css`
     font-size: 1.6rem;
     font-weight: 700;
     font-family: Pretendard;
-    line-height: 1.6;
+    line-height: 1.5;
   `,
   bold_15: css`
     font-size: 1.5rem;
     font-weight: 700;
     font-family: Pretendard;
-    line-height: 1.6;
+    line-height: 1.6666;
   `,
   semiBold_18: css`
     font-size: 1.8rem;
     font-weight: 600;
     font-family: Pretendard;
-    line-height: 1.5;
+    line-height: 1.5555;
   `,
   semiBold_16: css`
     font-size: 1.6rem;
     font-weight: 600;
     font-family: Pretendard;
-    line-height: 1.5;
+    line-height: 1.6;
   `,
   semiBold_15: css`
     font-size: 1.5rem;
     font-weight: 600;
     font-family: Pretendard;
-    line-height: 1.5;
+    line-height: 1.6;
   `,
   semiBold_14: css`
     font-size: 1.4rem;
     font-weight: 600;
     font-family: Pretendard;
-    line-height: 1.5;
+    line-height: 1.4285;
   `,
   semiBold_13: css`
     font-size: 1.3rem;
     font-weight: 600;
     font-family: Pretendard;
-    line-height: 1.5;
+    line-height: 1.2307;
   `,
   medium_15: css`
     font-size: 1.5rem;
@@ -65,30 +65,30 @@ export const font = {
     font-size: 1.4rem;
     font-weight: 500;
     font-family: Pretendard;
-    line-height: 1.6;
+    line-height: 1.4285;
   `,
   medium_13: css`
     font-size: 1.3rem;
     font-weight: 500;
     font-family: Pretendard;
-    line-height: 1.6;
+    line-height: 1.2307;
   `,
   regular_15: css`
     font-size: 1.5rem;
     font-weight: 400;
     font-family: Pretendard;
-    line-height: 1.5;
+    line-height: 1.6;
   `,
   regular_14: css`
     font-size: 1.4rem;
     font-weight: 400;
     font-family: Pretendard;
-    line-height: 1.428;
+    line-height: 1.4285;
   `,
   regular_12: css`
     font-size: 1.2rem;
     font-weight: 400;
     font-family: Pretendard;
-    line-height: 1.333;
+    line-height: 1.2307;
   `,
 };
