26f04076f388fcb6bf65779340bd63bb25da343a
choisuhyeok1255 <csh300029@gmail.com>
2023-04-04 21:23:20 +0900

    카카오 스크립트 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { RecoilRoot } from "recoil";
 
 import { Global, ThemeProvider } from "@emotion/react";
@@ -21,20 +20,11 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout || (page => page);
 
   return (
-    <>
-      <Head>
-        <script
-          src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
-          integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <Global styles={globalStyle} />
-        <RecoilRoot>{getLayout(<Component {...pageProps} />)}</RecoilRoot>
-        <div id="modal" />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyle} />
+      <RecoilRoot>{getLayout(<Component {...pageProps} />)}</RecoilRoot>
+      <div id="modal" />
+    </ThemeProvider>
   );
 };
 
