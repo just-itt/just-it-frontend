e002be4269835711b6ddf6570931169e2b6227ed
choisuhyeok1255 <csh300029@gmail.com>
2023-04-22 23:13:55 +0900

    axios timeout 설정, interceptors 함수 호출 방법 변경

--- apis/axios.ts
+++ apis/axios.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const ax = axios.create({ baseURL: process.env.NEXT_PUBLIC_BASE_URL });
+const ax = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  timeout: 3000,
+});
 
-ax.interceptors.request.use(function (config) {
+ax.interceptors.request.use(config => {
   const token = Cookies.get("auth");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
