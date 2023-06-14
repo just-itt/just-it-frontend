24d06e558abd9706c84aff0bad53549ec8dadf0f
choisuhyeok1255 <csh300029@gmail.com>
2023-06-14 23:02:24 +0900

    footer css 수정

--- components/common/footer/Footer.component.tsx
+++ components/common/footer/Footer.component.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import { FooterIcon } from "@icons/index";
 import * as S from "./Footer.styled";
@@ -8,15 +9,21 @@ const Footer = () => {
     <S.Footer>
       <FooterIcon />
       <S.NameWrapper>
-        <S.Name>
-          <S.Position>Front-end Developer</S.Position>Choi Su-hyeok
-        </S.Name>
-        <S.Name>
-          <S.Position>Back-end Developer</S.Position>Lee Ji-soo
-        </S.Name>
-        <S.Name>
+        <span>
+          <S.Position>Front-end Developer</S.Position>
+          <Link href="https://github.com/choisuhyeok1255" target="_blank">
+            Choi Su-hyeok
+          </Link>
+        </span>
+        <span>
+          <S.Position>Back-end Developer</S.Position>
+          <Link href="https://github.com/2jisu" target="_blank">
+            Lee Ji-soo
+          </Link>
+        </span>
+        <span>
           <S.Position>Prodect Designer</S.Position>Jeong Seung-hoon
-        </S.Name>
+        </span>
       </S.NameWrapper>
     </S.Footer>
   );
--- components/common/footer/Footer.styled.ts
+++ components/common/footer/Footer.styled.ts
@@ -4,29 +4,50 @@ import styled from "@emotion/styled";
 export const Footer = styled.footer`
   ${({ theme }) => css`
     ${theme.flexMixin.flex_column_justifyC_alignC};
-    ${theme.font.regular_14};
-    row-gap: 12px;
-    height: 105px;
+    ${theme.font.regular_12};
+    height: 149px;
     border-top: 1px solid ${theme.color.grey_200};
-    padding-top: 20px;
     color: ${theme.color.grey_500};
+
+    & > svg {
+      margin-bottom: 12px;
+    }
+
+    @media (min-width: ${theme.breakPoint.minTablet}) {
+      ${theme.flexMixin.flex_column_justifyC_alignC};
+      ${theme.font.regular_14};
+      row-gap: 12px;
+      height: 105px;
+      padding-top: 20px;
+
+      & > svg {
+        margin-bottom: 0;
+      }
+    }
   `}
 `;
 
 export const NameWrapper = styled.div`
   ${({ theme }) => css`
-    display: flex;
+    ${theme.flexMixin.flex_column_justifyC_alignC};
 
-    & > span:not(:last-child)::after {
-      content: "";
-      width: 1px;
-      margin: 20px;
-      border-right: 1px solid ${theme.color.grey_200};
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    @media (min-width: ${theme.breakPoint.minTablet}) {
+      display: block;
+
+      & > span:not(:last-child)::after {
+        content: "";
+        width: 1px;
+        margin: 20px;
+        border-right: 1px solid ${theme.color.grey_200};
+      }
     }
   `}
 `;
-
-export const Name = styled.span``;
 
 export const Position = styled.span`
   ${({ theme }) => css`
