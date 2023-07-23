add7b8b32b27e5b6da73ba3b74838665585a5003
choisuhyeok1255 <csh300029@gmail.com>
2023-07-23 23:18:12 +0900

    피드 헤더 zindex 추가

--- components/common/pheed/pheedHeader/PheedHeader.styled.ts
+++ components/common/pheed/pheedHeader/PheedHeader.styled.ts
@@ -11,6 +11,7 @@ export const Header = styled.header`
     border-bottom: 1px solid ${theme.color.grey_200};
     padding: 0 20px;
     background-color: ${theme.color.white};
+    z-index: ${theme.zIndex.header};
 
     @media (min-width: ${theme.breakPoint.minDesktop}) {
       position: relative;
