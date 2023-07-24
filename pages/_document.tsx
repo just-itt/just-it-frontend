97c90066d3074ce6fb7aa1ddf098a92db5ab2513
choisuhyeok1255 <csh300029@gmail.com>
2023-07-24 23:11:35 +0900

    네이버 서치 어드바이저 메타 태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,10 @@ const Document = () => {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
         />
+        <meta
+          name="naver-site-verification"
+          content="1acf413ed963d11f80583fa5826231aa86acaa23"
+        />
       </Head>
       <body>
         <Main />
