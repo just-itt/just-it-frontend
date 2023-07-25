6dec1cde01c6ed1a1a6c88a856e1408c0462f259
choisuhyeok1255 <csh300029@gmail.com>
2023-07-25 23:16:53 +0900

    Seo 컴포넌트 제작

---
+++ components/common/seo/Seo.component.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Head from "next/head";
+
+interface SeoProps {
+  title?: string;
+  description: string;
+  url?: string;
+  imgUrl?: string;
+}
+
+const Seo = ({ title, description, url, imgUrl }: SeoProps) => {
+  return (
+    <Head>
+      <title>{title ? `${title} | just it` : "just it"}</title>
+      <meta
+        name="description"
+        content="매일 똑같은 고민, 오늘 점심 뭐먹지? just it이 고민을 해결해줄게요! 나와 같은 상황일 때 다른 사람은 무엇을 먹을까요? 궁금하지 않나요? just it은 다양한 음식 메뉴를 추천하여 특별한 식사 시간을 만들어드립니다."
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:title"
+        content={title ? `${title} | just it` : "just it"}
+      />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url || "https://justit.kr"} />
+      <meta property="og:image" content={imgUrl || "https://justit.kr"} />
+      <meta property="og:article:author" content="just it" />
+      <meta property="og:locale" content="ko_KR" />
+      <meta property="og:site_name" content="just it" />
+    </Head>
+  );
+};
+
+export default Seo;
