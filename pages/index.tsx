5f75d3ff2ad0b8b4c0e6f48343b38d32ddcc78e6
choisuhyeok1255 <csh300029@gmail.com>
2023-07-26 20:22:20 +0900

    Home SEO 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,32 @@ import axios from "axios";
 import { QueryClient, dehydrate } from "@tanstack/react-query";
 import type { GetServerSidePropsContext } from "next";
 
-import { MainLayout, HomeContainer } from "@components/index";
+import { MainLayout, HomeContainer, Seo } from "@components/index";
 import { commonKeys, pheedKeys } from "@service/index";
 
-const index = () => {
-  return <HomeContainer />;
+interface HomeProps {
+  detailTitle?: string;
+  imgUrl?: string;
+  content?: string;
+}
+
+const Home = ({ detailTitle, imgUrl, content }: HomeProps) => {
+  return (
+    <>
+      <Seo
+        title={detailTitle}
+        description={
+          content ??
+          "매일 똑같은 고민, 오늘 점심 뭐먹지? just it이 고민을 해결해줄게요! 나와 같은 상황일 때 다른 사람은 무엇을 먹을까요? 궁금하지 않나요? just it은 다양한 음식 메뉴를 추천하여 특별한 식사 시간을 만들어드립니다."
+        }
+        imgUrl={imgUrl}
+      />
+      <HomeContainer />
+    </>
+  );
 };
 
-index.getLayout = function getLayout(page: ReactElement) {
+Home.getLayout = function getLayout(page: ReactElement) {
   return <MainLayout>{page}</MainLayout>;
 };
 
@@ -49,11 +67,30 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
     staleTime: Infinity,
   });
 
+  let detailTitle = "";
+  let imgUrl = "";
+  let content = "";
+
+  await queryClient.prefetchQuery({
+    queryKey: pheedKeys.pheedDetail({ id: query.id as string }),
+    queryFn: async () => {
+      const { data } = await ax.get(`/posts/${query.id}`);
+      detailTitle = data.title;
+      content = data.content;
+      imgUrl = data.image.image;
+
+      return data;
+    },
+  });
+
   return {
     props: {
+      detailTitle,
+      imgUrl,
+      content,
       dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
     },
   };
 }
 
-export default index;
+export default Home;
